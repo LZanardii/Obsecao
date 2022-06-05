403db4507afc0e59c27b3542ecfd457c5e541a79
Leonardo Zanardi <leonardo.szanardi@gmail.com>
2022-06-04 22:14:51 -0300

    inicial commit

---
+++ src/main/java/br/com/unisinos/obsecao/ObsecaoApplication.java
@@ -0,0 +1,13 @@
+package br.com.unisinos.obsecao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ObsecaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ObsecaoApplication.class, args);
+	}
+
+}
